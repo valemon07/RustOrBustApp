c8d5d94f97aae26a6accd87459ef42b14ec2a993
Andy V <andres.valeriomontero@gmail.com>
2026-03-17 15:03:42 -0400

    Class 3/17

--- src/Components/CenterPanel.jsx
+++ src/Components/CenterPanel.jsx
@@ -1,23 +1,29 @@
-import React from 'react';
-import "/src/assets/folder-open-solid-full.svg"
+import React from "react";
+import "/src/assets/folder-open-solid-full.svg";
 
-const FileUpload = ({className, onClick}) => {
-    return (
-        <div className={className} onClick={onClick}>
-            <img src="/src/assets/folder-open-solid-full.svg" alt="Folder Icon" style={{ width: '70px', height: '70px', marginBottom: '10px' }} />
-            <div>
-                Drag and drop files here, or click to browse your computer.
-            </div>
-        </div>
-    );
-}
+const FileUpload = ({ className, onClick }) => {
+  return (
+    <div className={className} onClick={onClick}>
+      <img
+        src="/src/assets/folder-open-solid-full.svg"
+        alt="Folder Icon"
+        style={{ width: "70px", height: "70px", marginBottom: "10px" }}
+      />
+      <div>Drag and drop files here, or click to browse your computer.</div>
+    </div>
+  );
+};
+
+const clickHandler = () => {
+  alert("File handling will be implemented here");
+};
 
 export default function CenterPanel() {
-    return (
-        <div>
-            <div className="outer-panel">
-                <FileUpload className="inner-panel" onClick={() => alert('File upload functionality will be implemented here.')}/>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="outer-panel">
+        <FileUpload className="inner-panel" onClick={() => clickHandler()} />
+      </div>
+    </div>
+  );
 }
