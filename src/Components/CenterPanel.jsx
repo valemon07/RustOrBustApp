b6332ee0840cec79880cc52672a2821dbedaad20
Andy V <andres.valeriomontero@gmail.com>
2026-04-02 15:01:06 -0400

    Cleanup

--- src/Components/CenterPanel.jsx
+++ src/Components/CenterPanel.jsx
@@ -2,28 +2,6 @@ import React from "react";
 import "/src/assets/folder-open-solid-full.svg";
 import FileUpload from "../lib/FileUpload";
 
-// const FileUpload = ({ className, onClick }) => {
-//   return (
-//     <div className={className} onClick={onClick}>
-//       <img
-//         src="/src/assets/folder-open-solid-full.svg"
-//         alt="Folder Icon"
-//         style={{ width: "70px", height: "70px", marginBottom: "10px" }}
-//       />
-//       <div>Drag and drop files here, or click to browse your computer.</div>
-//     </div>
-//   );
-// };
-
-// const clickHandler = async () => {
-//   const result = await window.electronAPI.openFile();
-//   if (!result.canceled) {
-//     // Prints result.filePaths (array) to test
-//     console.log('Selected files:', result.filePaths);
-//     sendFiles(result.filePaths);
-//   }
-// };
-
 
 
 export default function CenterPanel() {
